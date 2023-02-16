496f0bb371062bcd7fda43b673b65dc77ec38623
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-16 17:30:38 -0300

    feat: criação de interfaces

---
+++ src/interfaces/book.ts
@@ -0,0 +1,11 @@
+export default interface Book {
+    id?: number,
+    title: string,
+    rating?: number,
+    year?: number,
+    genre?: string,
+    pages?: number,
+    thumb?: string,
+    read: number,
+    in_my_list: number,
+}
