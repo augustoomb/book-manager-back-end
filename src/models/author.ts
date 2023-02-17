1069eb5ba3932d978a583da86de807ceea7b2a18
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-17 13:34:37 -0300

    criação da model de author

---
+++ src/models/author.ts
@@ -0,0 +1,32 @@
+import { RowDataPacket } from 'mysql2';
+import conn from '../database/connection';
+import Author from '../interfaces/author';
+import { SimpleModel } from './model';
+
+const DATABASE = 'book_manager_db'
+
+export default class AuthorModel implements SimpleModel<Author> {
+
+    constructor(private tableName: string = 'Authors', private connection = conn){}
+
+    async create(obj: Author): Promise<void> {
+        await this.connection.execute(`INSERT INTO ${DATABASE}.${this.tableName} (name)
+        VALUES (?);`, [obj.name])
+    }
+
+    async list(): Promise<Partial<Author>[]> {
+        const result = await this.connection.execute(`SELECT id, name
+         FROM ${DATABASE}.${this.tableName};`);
+
+        const [authors] = result;
+        return authors as Author[];
+    }
+
+    async find(id: number): Promise<Partial<Author> | null> {
+        const result = await this.connection.execute(`SELECT id, name
+         FROM ${DATABASE}.${this.tableName} AS A WHERE A.id = ?;`, [id]);
+
+        const [authors] = result as RowDataPacket[];
+        return authors[0] as Author;
+    }
+}
---
+++ src/models/model.ts
@@ -0,0 +1,16 @@
+export interface SimpleModel<T> {
+    create(obj: T): Promise<void>;
+    list(): Promise<Partial<T>[]>;
+    find(id: number): Promise<Partial<T> | null>;
+}
+export interface Model<T> extends SimpleModel<T> {
+    update(id: number, obj: T): Promise<void>;
+    delete(id: number): Promise<void>;
+}
+
+// export interface ComplexModel<T> extends SimpleModel<T> {
+//     findByCast(id: number): Promise<T | null>;
+//     findByCharacter(id: number): Promise<T | null>;
+// }
+
+// partial tira a obrigação de retornar o obj completo
