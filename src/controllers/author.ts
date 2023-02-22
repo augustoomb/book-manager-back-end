779b605913f946be2f3bc57713e5759a029a5f17
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-22 08:35:39 -0300

    fix: implementando o uso do express async errors e removendo try/catch

--- src/controllers/author.ts
+++ src/controllers/author.ts
@@ -1,47 +1,54 @@
 import { NextFunction, Request, Response } from 'express';
-import BadRequest from '../errors/badRequest';
+// import BadRequest from '../errors/badRequest';
 import NotFound from '../errors/notFound';
 import AuthorInterface from '../interfaces/author';
 import { AuthorService } from '../services/author';
 
-export async function create(req: Request, res: Response, next: NextFunction) {
-  const { name, site } = req.body as AuthorInterface;
-  const authorService = new AuthorService();
-  try {
-    if (name === undefined) {
-      throw new BadRequest('Você precisa enviar o nome do autor');
-    }
-    await authorService.create({ name, site });
-    res.status(201).send();
-  } catch (err) {
+export async function create(req: Request, res: Response, _next: NextFunction) {
+  const { name, site } = req.body
 
-    next(err);
-  }
+  const authorService = new AuthorService();
+  const objAuthor = AuthorInterface.parse({ name, site })
+  await authorService.create(objAuthor);
+  res.status(201).send();
 }
 
-export async function find(req: Request, res: Response, next: NextFunction) {
+export async function find(req: Request, res: Response, _next: NextFunction) {
   const { id } = req.params;
   const authorService = new AuthorService();
-  try {
-    if (id === undefined) throw new BadRequest('Você precisa enviar o id da pesquisa');
+  
+  // if (id === undefined) throw new BadRequest('Você precisa enviar o id da pesquisa');
 
-    const obj = await authorService.find(parseInt(id, 10));
+  const obj = await authorService.find(parseInt(id, 10));
+  if (!obj) throw new NotFound('Autor não encontrado');
 
-    if (!obj) throw new NotFound('Autor não encontrado');
+  return res.status(200).json(obj);
 
-    return res.status(200).json(obj);
-  } catch (err) {
-    next(err);
-  }
+  // try {
+  //   if (id === undefined) throw new BadRequest('Você precisa enviar o id da pesquisa');
+
+  //   const obj = await authorService.find(parseInt(id, 10));
+
+  //   if (!obj) throw new NotFound('Autor não encontrado');
+
+  //   return res.status(200).json(obj);
+  // } catch (err) {
+  //   next(err);
+  // }
 }
 
-export async function list(_req: Request, res: Response, next: NextFunction) {
+export async function list(_req: Request, res: Response, _next: NextFunction) {
   const authorService = new AuthorService();
-  try {
-    const authorList = await authorService.list();
 
-    return res.json(authorList);
-  } catch (err) {
-    next(err);
-  }
+  const authorList = await authorService.list();
+
+  return res.json(authorList);
+
+  // try {
+  //   const authorList = await authorService.list();
+
+  //   return res.json(authorList);
+  // } catch (err) {
+  //   next(err);
+  // }
 }
