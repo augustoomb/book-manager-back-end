ac34cc76f993fbb8eb1fdd1be7f5cd78de2619c5
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-22 10:06:49 -0300

    a class service Book não precisa mais fazer validação. Agora a validação é feita diretamente no tipo, usando o zod

--- src/services/book.ts
+++ src/services/book.ts
@@ -2,18 +2,18 @@ import Book from '../interfaces/book';
 import { Model } from '../models/model';
 import BookModel from '../models/book';
 import Service from './service';
-import BadRequest from '../errors/badRequest';
+// import BadRequest from '../errors/badRequest';
 
 export class BookService extends Service<Book> {
   constructor(model: Model<Book> = new BookModel()) {
     super(model);
   }
 
-  async create(obj: Book): Promise<void> {
-    if (obj.title.length <= 3) {
-      throw new BadRequest('O título precisa ter pelo menos 4 caracteres');
-    }
-    return super.create(obj);
-  }
+  // async create(obj: Book): Promise<void> {
+  //   if (obj.title.length <= 3) {
+  //     throw new BadRequest('O título precisa ter pelo menos 4 caracteres');
+  //   }
+  //   return super.create(obj);
+  // }
 
 }
