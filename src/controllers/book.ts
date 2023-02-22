f0e3cb40532ba9c532f3f6f0c74983ba9316dcba
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-22 08:36:48 -0300

    fix: removendo validações manuais de book.ts, pois foi implementando o Zod para validações diretamente no tipo

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -5,12 +5,16 @@ import BookInterface from '../interfaces/book';
 import { BookService } from '../services/book';
 
 export async function create(req: Request, res: Response, next: NextFunction) {
-  const { title, rating, year,
-    genre, pages, thumb, hasBeenRead, authorId } = req.body as BookInterface;
+  const { title, rating=0, year=0,
+    genre='nao-definido', pages=0, thumb='nao-definido', hasBeenRead=0, authorId
+   } = req.body as BookInterface;
   const bookService = new BookService();
   try {
     if (title === undefined) {
       throw new BadRequest('Você precisa enviar o título do livro');
+    }
+    if (authorId === undefined) {
+      throw new BadRequest('Você precisa enviar o id do autor');
     }
     await bookService.create({ title, rating, year, genre, pages, thumb, hasBeenRead, authorId });
     res.status(201).send();
@@ -48,22 +52,22 @@ export async function list(_req: Request, res: Response, next: NextFunction) {
 }
 
 export async function update(req: Request, res: Response, next: NextFunction) {
-    const { title, rating, year, genre, pages,
-      thumb, hasBeenRead, authorId } = req.body as BookInterface;
+  const { title, rating=0, year=0,
+    genre='nao-definido', pages=0, thumb='nao-definido', hasBeenRead=0, authorId
+   } = req.body as BookInterface;
     const { id } = req.params;
     const bookService = new BookService();
     try {
-      if (id === undefined) {
-        throw new BadRequest('Você precisa enviar o id do livro');
-      }
       if (title === undefined) {
         throw new BadRequest('Você precisa enviar o título do livro');
+      }
+      if (authorId === undefined) {
+        throw new BadRequest('Você precisa enviar o id do autor');
       }
       await bookService.update(Number(id), { title, rating, year, genre,
         pages, thumb, hasBeenRead, authorId });
       res.status(201).send();
-    } catch (err) {
-  
+    } catch (err) {  
       next(err);
     }
   }
@@ -75,7 +79,6 @@ export async function update(req: Request, res: Response, next: NextFunction) {
       await bookService.delete(Number(id));
       res.status(201).send();
     } catch (err) {
-  
       next(err);
     }
   }
