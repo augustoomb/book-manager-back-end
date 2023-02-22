17da8387572b718248f4a829db94204a2e633306
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-22 08:37:46 -0300

    fix: implementando o zod no tipo Author

--- src/interfaces/author.ts
+++ src/interfaces/author.ts
@@ -1,5 +1,20 @@
-export default interface Author {
-    id?: number,
-    name: string,
-    site?: string,
-}
+import { z } from 'zod';
+
+const Author = z.object({
+    id: z.number({
+        'invalid_type_error': '"id" deve ser um número',
+    }).optional(),
+    name: z.string({
+        'required_error': '"name" é um campo obrigatório',
+        'invalid_type_error': '"name" deve ser uma string',
+    }).min(3,{ 
+        message: '"name" deve ter no mínimo 3 caracteres' 
+    }),
+    site: z.string({
+        'invalid_type_error': '"site" deve ser uma string',
+    }).optional(),
+})
+
+type Author = z.infer<typeof Author>
+
+export default Author
